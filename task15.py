9bd3c1916ca66bf9085f0b2eefaf6d1a07103f51
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-03-12 12:56:42 +0300

    Add task15

---
+++ task15.py
@@ -0,0 +1,14 @@
+"""Найти вес самого легкого и самого тяжелого арбуза
+"""
+from random import randrange
+N = int (input ( "Введите количество арбузов = "))
+max = 0
+min = 20
+for i in range(N):
+    a = randrange(0,20)
+    print(a, end =" ")
+    if a > max:
+        max = a
+    elif a < min:
+        min = a
+print (f"максимальный вес - {max}, Минимальный вес - {min}")        
